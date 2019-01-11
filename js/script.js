668cad3be2eb78c097866db0082de2b4828765bd
An <an.arletta.cattie@gmail.com>
2019-01-11 15:41:30 +0100

    no message

--- js/script.js
+++ js/script.js
@@ -1,70 +1,45 @@
 // Solution goes here
 $(document).ready(function(){
     
-// submit-button
-$("send").on("click",function(){
-    
-})
-function check() {
-    var name= $('.form-control').val();
-    if(name){
-        if(name.length>5){
-            for(var i=0;i<name.length;i++) {
-                if(name.length<5){
-                    $('#mistake').text('Please enter your name - at least 5 characters ');
-                    $('#mistake').css('color','red');
-                    return false;
-                }
-            }
-                alert('form is going to be submitted');
-                $(".form-control").css("color", "green");
-                return true;
+// email
+let testEmail = /^[A-Z0-9.._%+-]+@(([A-Z0-9-])+\.)+[A-Z]{2,4}$/i;
+
+// function less then 5 characters 
+    function checkl() {
+        let name = $("#display-name").val();
+        let emailinput = $("#email").val();
+        if(name.length<5){
+            $("#display-name").css("border-color","red");
         }
-    }
-     $('#error').text('Name cant be blank');
-     $('#error').css('display','inline-block');
-    return false;
-}
-    // function less then 5 characters 
-    function check() {
-        var name= $('#display-name').val();
-            if(name.length<5){
-                $("#display-name").css("color","red");
-                alert($("#mistake"));
-                    }
-            else(
-                $("#display-name").css("color","green")
-            );
-            };
-
-    // function passwordvalidation
-        function passwordconfirmation() {
-            let password = $("#pass").val();
-            let confirmPassword = $("#pass-confirm").val();
-            if (password != confirmPassword) {
-                alert("Passwords do not match.");
-                return false;
-            }
-            else (
-                alert("Passwords are correct")
-            );
+        else{
+            $("#display-name").css("border-color","green")
         };
+        if (testEmail.test(emailinput)){
+        $("#email").css("border-color","green");
+        }
+        else {
+            $("#email").css("border-color","red");
+        }
+    };
 
-    // function for empty input
-        $("#send").click(function(){
-            if($(".form-control").val() == ""){
-                alert('Input can not be left blank');
-            }
-        });
+// function passwordvalidation
+    function passwordconfirmation() {
+        let password = $("#pass").val();
+        let confirmPassword = $("#pass-confirm").val();
+        if (password != confirmPassword || password === "" || password.length<6){
+            $("#mistake").show();
+            $("#pass, #pass-confirm").css("border-color","red");
+        }
+        else{
+            $("#mistake").hide();
+            $("#pass, #pass-confirm").css("border-color","green");
+        }
+    
+    };
 
-    // function for reset button 
-        $('#reset').click(function(e){
-            $(".form-control")[0].reset();
-        });
-
-
-
-
-
-
-});
+ $("#send").on("click",function(event){
+     event.preventDefault()
+     passwordconfirmation();
+     checkl();
+ });
+});     
